9218c0809f1bc2c8ef113c2243366238e2e7690a
Ali Pouryousefi <a.pouryousefi98@gmail.com>
2025-10-15 12:34:39 +0330

    add Cocktails component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import gsap from 'gsap'
-import { ScrollTrigger, SplitText } from 'gsap/all'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero';
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
 
-gsap.registerPlugin(ScrollTrigger,SplitText);
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
-
-      
+      <Cocktails />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
